70e2b3967518a22d93c7742d43301a51de4da78a
DinosaurusRexia <clark.li325@gmail.com>
2020-08-14 18:26:01 -0400

    cache

--- Deluxe_Consignment/store/admin.py
+++ Deluxe_Consignment/store/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from .models import *
 
+admin.site.site_header = "Deluxe Consignment Shop Admin Dashboard"
+
 # Register your models here.
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
 
 admin.site.register(Shoe)
 admin.site.register(Bag)
--- Deluxe_Consignment/store/urls.py
+++ Deluxe_Consignment/store/urls.py
@@ -4,7 +4,6 @@ from . import views as store_views
 urlpatterns = [
     path('', store_views.home, name='home'),
     path('store', store_views.store, name='store'),
-    path('products', store_views.products, name='products'),
     path('consign', store_views.consign, name='consign'),
     path('about', store_views.about, name='about'),
     path('paymentPolicy', store_views.paymentPolicy, name='paymentPolicy')
