7a70e1380abce2de0957ba9b790077705fe25edb
unknown <jimmyy1337@gmail.com>
2020-08-14 19:22:54 -0400

    Added home features

--- Deluxe_Consignment/static/js/main.js
+++ Deluxe_Consignment/static/js/main.js
@@ -1,1 +1,11 @@
 M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    let elems = document.querySelectorAll('.carousel');
+    let items = document.querySelectorAll('.carousel-item');
+    // items.forEach();
+    let instances = M.Carousel.init(elems, {
+        numVisible: 3,
+        dist: -50,
+    });
+});
