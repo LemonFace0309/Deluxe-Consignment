a31add8a4c94e91d7e182d97c7c9b3dfa0bbceea
DinosaurusRexia <clark.li325@gmail.com>
2020-08-14 15:32:03 -0400

    updated models

--- Deluxe_Consignment/store/models.py
+++ Deluxe_Consignment/store/models.py
@@ -1,12 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Product(models.Model):
     name = models.TextField(default="a")
-    price = models.IntegerField();
+    price = models.IntegerField(default=1)
+    thumbnail = models.ImageField(null=True, blank=True)
+    quantity = models.IntegerField(default=1)
+    description = models.TextField(max_length=2000)
+
 
 class Shoe(Product, models.Model):
     size = models.IntegerField(default=10)
 
+
 class Bag(Product, models.Model):
     pass
+
+
+class Jewelery(Product, models.Model):
+    pass
+
+
+class Accessories(Product, models.Model):
+    pass
+
+
+class SLGS(Product, models.Model):
+    pass
+
+
