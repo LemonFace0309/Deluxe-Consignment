4ac88b62b9f4001ab169db09b41625b6f6573f98
unknown <jimmyy1337@gmail.com>
2020-08-19 04:11:07 -0400

    Functional navbar

---
+++ Deluxe_Consignment/static/js/nav.js
@@ -0,0 +1,26 @@
+const menuOpen = document.querySelector('#menu-open');
+const menuClose = document.querySelector('#menu-close');
+const sideNav = document.querySelector('.left-nav');
+const searchBar = document.querySelector('#searchBar');
+
+menuOpen.addEventListener('click', () =>  {
+    console.log("asd");
+    sideNav.classList.add('toggle');
+    document.querySelector('#menu-open').style.color = 'green';
+    document.body.style.overflow = "hidden";
+});
+
+menuClose.addEventListener('click', () => {
+    sideNav.classList.remove('toggle');
+    document.body.style.overflow = "auto";
+});
+
+searchBar.addEventListener('intput', () => {
+
+});
+
+window.addEventListener('scroll', function() {
+    let nav = document.querySelector('nav');
+    nav.classList.toggle('sticky', window.scrollY > window.innerHeight-150);
+})
+
