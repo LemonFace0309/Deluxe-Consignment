07e3e45f402eee637a5c7f99477b97a0ff3a101b
unknown <jimmyy1337@gmail.com>
2020-08-20 01:28:31 -0400

    Added sidenav functionality

--- Deluxe_Consignment/static/js/nav.js
+++ Deluxe_Consignment/static/js/nav.js
@@ -1,3 +1,4 @@
+const nav = document.querySelector('nav');
 const menuOpen = document.querySelector('#menu-open');
 const menuClose = document.querySelector('#menu-close');
 const sideNav = document.querySelector('.left-nav');
@@ -5,13 +6,15 @@ const searchBar = document.querySelector('#searchBar');
 
 menuOpen.addEventListener('click', () =>  {
     console.log("asd");
-    sideNav.classList.add('toggle');
-    document.querySelector('#menu-open').style.color = 'green';
+    sideNav.classList.add('showSide');
+    // document.querySelector('#menu-open').style.color = 'green';
     document.body.style.overflow = "hidden";
 });
 
 menuClose.addEventListener('click', () => {
-    sideNav.classList.remove('toggle');
+    sideNav.classList.remove('showSide');
+    sideNav.classList.add('hideSide');
+    setTimeout(() => sideNav.classList.remove('hideSide'), 400);
     document.body.style.overflow = "auto";
 });
 
@@ -20,11 +23,23 @@ searchBar.addEventListener('intput', () => {
 });
 
 window.addEventListener('scroll', function() {
-    let nav = document.querySelector('nav');
+    // console.log({{request.path}});
     if (!window.location.href.includes('store')) {
         nav.classList.toggle('sticky', window.scrollY > window.innerHeight-150);
     } else {
         nav.classList.toggle('sticky', window.scrollY > 100);
     }
-})
+    resizeNav();
+});
 
+
+const navBack = document.querySelector('.nav-back');
+function resizeNav() {
+    navBack.style.height = `${nav.clientHeight}px`;
+} 
+window.addEventListener('resize', resizeNav);
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    resizeNav();
+});
