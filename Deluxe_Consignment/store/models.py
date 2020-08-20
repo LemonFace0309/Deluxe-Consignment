db707096379eaf1dcb7565cd4bdb6bcd98b35feb
DinosaurusRexia <clark.li325@gmail.com>
2020-08-20 14:14:50 -0400

    migrations and cache

--- Deluxe_Consignment/store/models.py
+++ Deluxe_Consignment/store/models.py
@@ -53,7 +53,7 @@ class Product(models.Model):
 
 
 class ProductImage(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     image = models.ImageField(null=True, blank=True)
 
 
