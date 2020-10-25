be62012c5c39a888fd38cd1a7c026cdea785ad86
Jimmy Yang <jimmyy1337@gmail.com>
2020-10-25 17:27:36 -0400

    Blog model skeleton

--- Deluxe_Consignment/blog/models.py
+++ Deluxe_Consignment/blog/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Blog(models.Model):
-    name = models.CharField(max_length=100, default="name")
+    title = models.CharField(max_length=100, default="title")
