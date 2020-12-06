f673d8ebf69d48913f41d06edce7b82b6c8cf40e
Jimmy Yang <jimmyy1337@gmail.com>
2020-12-06 04:17:09 -0500

    Updated Blog view, floored footer, update nav-grid

--- Deluxe_Consignment/blog/models.py
+++ Deluxe_Consignment/blog/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=100, default="title")
+    title = models.CharField(max_length=100, default="Title")
+    content = models.TextField(max_length=5000, default="Content")
+    date_created = models.DateTimeField(default=timezone.now)
     
